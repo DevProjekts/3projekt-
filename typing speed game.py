ad899a22effc57144a702426810da7620dad1c03
DevProjekts <88111594+DevProjekts@users.noreply.github.com>
2021-08-02 13:12:57 +0200

    Update typing speed game.py

--- typing speed game.py
+++ typing speed game.py
@@ -1,9 +1,10 @@
 from time import time
 
+# calculate the accuracy of input prompt
 def typingErrors(prompt):
     global iwords
 
-    words = prompt.slit()
+    words = prompt.split()
     errors = 0
 
     for i in range(len(iwords)):
@@ -22,7 +23,7 @@ def typingErrors(prompt):
                 errors += 1
     return errors
 
-
+# calculate the speed in words per minute
 def typingSpeed(iprompt, stime, etime):
     global time
     global iwords
@@ -33,7 +34,7 @@ def typingSpeed(iprompt, stime, etime):
 
     return speed
 
-
+# calculate total time elapsed
 def timeElapsed(stime, etime):
     time = etime - stime
 
@@ -43,20 +44,20 @@ if __name__ == '__main__':
     prompt = "Long years ago we made a tryst with destiny, and now the time comes when we shall redeem our pledge, not wholly or in full measure, but very substantially. At the stroke of the midnight hour, when the world sleeps, India will awake to life and freedom. A moment comes, which comes but rarely in history, when we step out from the old to the new, when an age ends, and when the soul of a nation, long suppressed, finds utterance. It is fitting that at this solemn moment, we take the pledge of dedication to the service of India and her people and to the still larger cause of humanity."
     print("Type this:- '", prompt, "'")
 
-    
+    # listening to input ENTER
     input("press ENTER when you are ready!")
 
-    
+    # recording time for input
     stime = time()
     iprompt = input()
     etime = time()
 
-    
+    # gather all the information returned from functions
     time = round(timeElapsed(stime, etime), 2)
     speed = typingSpeed(iprompt, stime, etime)
     errors = typingErrors(prompt)
 
-   
+    # printing all the required data
     print("Total Time elapsed : ", time, "s")
     print("Your Average Typing Speed was : ", speed, "words / minute")
     print("With a total of : ", errors, "errors")
